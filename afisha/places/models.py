9f87ea2a401539d552f2abba05ab016912343fba
MSMikl <michalbl4@gmail.com>
2022-07-15 09:15:45 +0300

    Place model added

--- afisha/places/models.py
+++ afisha/places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200, blank=False)
+    description_short = models.TextField('Краткое описание', blank=True)
+    description_long = models.TextField('Полное описание', blank=True)
+    longitude = models.FloatField('Долгота', null=True)
+    lattitude = models.FloatField('Широта', null=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
